b826ed47cba85ca4b409d0fef28c13c1d822239e
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2021-05-11 17:45:08 +0700

    Fixed experimental ClipSVC mode
    0.5b Beta

--- DevCheck.xaml.cs
+++ DevCheck.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
+using Microsoft.Win32;
 
 namespace BlueSky
 {
@@ -28,7 +29,7 @@ namespace BlueSky
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Secret Developer Options is some set of advanced testing and experimenting features of BlueSky.");
+            MessageBox.Show("Developer Options is some set of advanced testing and experimenting features of BlueSky.");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -41,6 +42,12 @@ namespace BlueSky
             }
             else
             {
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\ClipSVC\Parameters", true);
+
+                //adding/editing a value 
+                key.SetValue("ServiceDll", @"%SystemRoot%\System32\ClipSVC.dll" + new Random().Next(1, 1000), RegistryValueKind.ExpandString); //sets 'someData' in 'someValue' 
+
+                key.Close();
                 string path;
                 path = @"C:\Windows\System32\Windows.ApplicationModel.Store.dll";
                 System.Diagnostics.Process takeperm1 = System.Diagnostics.Process.Start("cmd.exe", @"/c takeown /f " + path + @" && icacls " + path + " /grant %username%:F");
@@ -60,12 +67,34 @@ namespace BlueSky
             }
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            string strCmdText;
+            strCmdText = @"/C net stop ""ClipSVC"" ";
+            System.Diagnostics.Process killsvc = System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            killsvc.WaitForExit();
+            string mc1;
+            mc1 = @"/C explorer.exe shell:appsFolder\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App";
+            System.Diagnostics.Process mcst = System.Diagnostics.Process.Start("CMD.exe", mc1);
+            mcst.WaitForExit();
             var j = new ProcessMemoryReader();
             var processname = Process.Start("CMD.exe", @"/C explorer.exe shell:appsFolder\Microsoft.MinecraftUWP_8wekyb3d8bbwe!App");
             j.ReadProcess = processname;
             j.OpenProcess();
+            await Task.Delay(45000);
+            string mc2 = @"/C taskkill /IM ""RuntimeBroker.exe"" /F";
+            System.Diagnostics.Process kill1 = System.Diagnostics.Process.Start("CMD.exe", mc2);
+            kill1.WaitForExit();
+            RegistryKey key1 = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\ClipSVC\Parameters", true);
+
+            //adding/editing a value 
+            key1.SetValue("ServiceDll", @"%SystemRoot%\System32\ClipSVC.dll", RegistryValueKind.ExpandString); //sets 'someData' in 'someValue' 
+
+            key1.Close();
+            string clst1;
+            clst1 = @"/C net start ""ClipSVC"" ";
+            System.Diagnostics.Process.Start("cmd.exe", clst1);
+            MessageBox.Show("Game started successfully!");
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
