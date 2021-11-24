4cb0c7a44a2bb26503c1800208e5395a8998fbd5
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2021-11-25 06:53:15 +0700

    injection test code
    code included, not currently tested

---
+++ Libaries/test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BlueSky.Libaries;
+
+namespace BlueSky.Libaries
+{
+    internal class test
+    {
+        public static void start()
+        {
+            STARTUPINFO si = new STARTUPINFO();
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            bool success = NativeMethods.CreateProcess("minecraft://", null,
+                IntPtr.Zero, IntPtr.Zero, false,
+                ProcessCreationFlags.CREATE_SUSPENDED,
+                IntPtr.Zero, null, ref si, out pi);
+            Process[] s = Process.GetProcessesByName("Minecraft.Windows");
+            int sid = s.Length;
+            int id = s[sid - 1].Id;
+            sid -= 1;
+            injector.Inject((uint)id, AppDomain.CurrentDomain.BaseDirectory + "\\Assets\\LaunchingAsset\\1164.dll");
+            Thread.Sleep(200);
+
+            NativeMethods.ResumeThread(pi.hThread);
+        }
+    }
+}
